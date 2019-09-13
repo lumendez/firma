d0665ec65a67d3e6074257169baa5a802bdf5605
luis <luis@localhost.localdomain>
2019-09-12 19:55:32 -0700

    Formulario para registro de usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:nombre, :apellido_paterno, :apellido_materno, :role_id)
+      params.require(:user).permit(:nombre, :apellido_paterno, :apellido_materno, :role_id,
+      :password, :password_confirmation, :email)
     end
 end
