f2ff08ac73c99704465b564f68799f9e9b379177
luis <luis@localhost.localdomain>
2019-09-16 17:17:10 -0500

    Se agregaron datos de inicio a seed

--- app/models/constancia_documento.rb
+++ app/models/constancia_documento.rb
@@ -2,6 +2,6 @@ class ConstanciaDocumento < ApplicationRecord
   validates :folio, :numero_relacion,
     :numero_oficio, :numero_registro, :codigo_prestatario, :clave_programa,
     :fecha, :nombre, :boleta, :unidad_academica, :programa_academico,
-    :periodo, :prestatario, :constancia_emitida, :apellido_paterno,
+    :periodo, :prestatario, :apellido_paterno,
     :apellido_materno, presence: true
 end
