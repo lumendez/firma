2148ec71589d7fe28cdf95417531a59b5223ac1f
luis <luis@localhost.localdomain>
2019-10-04 00:00:31 -0500

    La funcion para firmar paso del modelo al controlador

--- app/controllers/constancia_documentos_controller.rb
+++ app/controllers/constancia_documentos_controller.rb
@@ -98,37 +98,7 @@ class ConstanciaDocumentosController < ApplicationController
   end
 
   def firmar
-    public_key = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('public.pem')))
-    constancia_documentos = ConstanciaDocumento.find(params[:constancia_documento_ids])
-    constancia_documentos.each do |constancia_documento|
-      folio = constancia_documento.folio
-      numero_relacion = constancia_documento.numero_relacion
-      numero_oficio = constancia_documento.numero_oficio
-      numero_registro = constancia_documento.numero_registro
-      codigo_prestatario = constancia_documento.codigo_prestatario
-      clave_programa = constancia_documento.clave_programa
-      fecha = constancia_documento.fecha
-      nombre = constancia_documento.nombre
-      boleta = constancia_documento.boleta
-      unidad_academica = constancia_documento.unidad_academica
-      programa_academico = constancia_documento.programa_academico
-      periodo = constancia_documento.periodo
-      prestatario = constancia_documento.prestatario
-      cadena = '||' + folio + '|' + numero_relacion + '|' + numero_oficio + '|' +
-      numero_registro + '|' + codigo_prestatario + '|' + clave_programa + '|' +
-      fecha + '|' + nombre + '|' + boleta + '|' + unidad_academica + '|' + programa_academico + '||'
-      firma = public_key.public_encrypt(cadena)
-      firma_electronica = Base64.encode64(firma)
-      constancia_documento.update(firma_direccion: firma_electronica)
-=begin
-  #Para el descifrado
-  private_key = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('private.pem')))
-  reg = @constancia_documento
-  info = reg.firma_direccion.gsub("\n","")
-  infobin = Base64.decode64(info)
-  private_key.private_decrypt(infobin)
-=end
-    end
+    ConstanciaDocumento.firmar(params[:constancia_documento_ids])
     redirect_to constancia_documentos_path, notice: 'La(s) constancia(s) ha(n) sido firmada(s) correctamente.'
   end
 
