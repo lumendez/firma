fe75bcac5050d1688615387a7ae74e1c4b68663b
luis <luis@localhost.localdomain>
2019-12-05 11:55:36 -0800

    Modificacion de la configuracion del mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.time_zone = 'America/Mexico_City'
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'serviciosocialconstancias.ipn.mx', port: 3000 }
 end
