307693f35323248b1c113a80195a86c3ade6b9d0
luis <luis@localhost.localdomain>
2019-12-05 18:29:03 -0800

    Nueva configuracion de mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,4 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.time_zone = 'America/Mexico_City'
   config.action_mailer.default_url_options = { host: 'serviciosocialconstancias.ipn.mx', port: 443 }
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
 end
