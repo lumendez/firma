b2761312a86d9c75a2e61f70318d225b586dcde9
luis <luis@localhost.localdomain>
2020-01-06 18:15:43 -0800

    constancia como adjunto

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,14 @@ class UserMailer < ApplicationMailer
 
   def email_notificacion(constancia_documento)
     @constancia_documento = constancia_documento
+
+    archivo = render_to_string_with_wicked_pdf(
+      pdf: "constancia.pdf",
+      template: "constancia_documentos/imprimir.html.erb",
+      layout: "imprimir.html.erb"
+      )
+
+    attachments["constancia.pdf"] = archivo
     mail(to: constancia_documento.correo, subject: "Constancia de Servicio Social")
   end
 end
