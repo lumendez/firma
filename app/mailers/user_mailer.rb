17e250e52d7ccfae92c2e8d771108d6859908eaf
luis <luis@localhost.localdomain>
2020-01-13 19:11:42 -0800

    Nuevo formato de constancia

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,13 +5,11 @@ class UserMailer < ApplicationMailer
   def email_notificacion(constancia_documento)
     @constancia_documento = constancia_documento
 
-    archivo = render_to_string_with_wicked_pdf(
-      pdf: "constancia.pdf",
-      template: "constancia_documentos/imprimir.html.erb",
-      layout: "imprimir.html.erb"
+    archivo = WickedPdf.new.pdf_from_string(
+      render_to_string(template: "constancia_documentos/imprimir.html.erb", layout: "imprimir.html.erb")
       )
 
-    attachments["constancia.pdf"] = archivo
+    attachments["constancia_documento.pdf"] = archivo
     mail(to: constancia_documento.correo, subject: "Constancia de Servicio Social")
   end
 end
