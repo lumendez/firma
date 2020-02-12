95744f07174a2845e0f5ff1469914fb2b3229ef9
luis <luis@localhost.localdomain>
2020-02-12 11:34:22 -0600

    Se cambio la zona horaria de produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'dctss@ipn.mx'}
+  config.time_zone = 'America/Mexico_City'
+  config.action_mailer.default_url_options = { host: 'serviciosocialconstancias.ipn.mx', port: 443 }
 end
