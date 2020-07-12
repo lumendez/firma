5cb0b2f91a3e194cdf7dfde6bedcf7e6e96c7609
luis <luis@localhost.localdomain>
2020-07-12 15:43:15 -0500

    Complemento julio

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,32 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib','seeds','datos600.csv'))
+csv_text = File.read(Rails.root.join('lib','seeds','constanciasjulio.csv'))
 csv = CSV.parse(csv_text, headers: true)
 
 csv.each do |row|
   t = ConstanciaDocumento.new
 
-  t.numero_registro = row['REGISTRO']
-  t.id = row['ID']
-  t.numero_relacion = row['RELACION']
-  t.numero_oficio = row['OFICIO']
-  t.codigo_prestatario = row['CLAVE']
-  t.clave_programa = row['PROGRAMA']
-  t.fecha = row['FECHA']
-  t.nombre = row['NOMBRE']
-  t.apellido_paterno = row['PATERNO']
-  t.apellido_materno = row['MATERNO']
-  t.boleta = row['BOLETA']
-  t.unidad_academica = row['UNIDAD ACADEMICA']
-  t.programa_academico = row['CARRERA']
-  t.periodo = row['FECHA TEXTO']
-  t.prestatario = row['PRESTATARIO']
-  t.constancia_emitida = row['EMITIDA']
-  t.user_id = row['USERID']
-  t.created_at = Time.now
-  t.updated_at = Time.now
+  t.id = row['id']
+  t.numero_relacion = row['numero_relacion']
+  t.numero_oficio = row['numero_oficio']
+  t.numero_registro = row['numero_registro']
+  t.codigo_prestatario = row['codigo_prestatario']
+  t.clave_programa = row['clave_programa']
+  t.fecha = row['fecha']
+  t.nombre = row['nombre']
+  t.boleta = row['boleta']
+  t.unidad_academica = row['unidad_academica']
+  t.programa_academico = row['programa_academico']
+  t.periodo = row['periodo']
+  t.prestatario = row['prestatario']
+  t.constancia_emitida = row['constancia_emitida']
+  t.created_at = row['created_at']
+  t.updated_at = row['updated_at']
+  t.apellido_paterno = row['apellido_paterno']
+  t.apellido_materno = row['apellido_materno']
+  t.user_id = row['user_id']
+  t.uuid = row['uuid']
+  t.correo = row['correo']
 
   t.save
 end
