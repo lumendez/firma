154bf49c6ef92bd66dfb89f2cccc6f310274a212
luis <luis@localhost.localdomain>
2020-08-20 13:22:26 -0500

    bloque de boletas dos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib','seeds','bloqueunoboletas.csv'))
+csv_text = File.read(Rails.root.join('lib','seeds','bloqueboletasdos.csv'))
 csv = CSV.parse(csv_text, headers: true)
 
 csv.each do |row|
@@ -8,6 +8,6 @@ csv.each do |row|
   id = row['ID']
   boleta = row['BOLETA']
   documento = ConstanciaDocumento.where(id: id)
-  documento.update(boleta: boleta)
+  documento.update(boleta: boleta, firma_direccion: nil, firma_departamento: nil)
 
 end
