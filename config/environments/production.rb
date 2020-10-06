7393454a03ff0a0bd3d25bca56e71e1afd1f427e
luis <luis@localhost.localdomain>
2020-10-06 14:50:22 -0500

    Cambios para la DCyC

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.time_zone = 'America/Mexico_City'
+  config.action_mailer.smtp_settings = {
+    address: 'emailfilter.activedirectory.ipn.mx'
+  }
 end
