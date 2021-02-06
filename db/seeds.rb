a3b376dcbf9b5f5a3e987fb4b118c96b1712b73e
luis <luis@localhost.localdomain>
2021-02-06 16:42:14 -0600

    Relacion 202

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib','seeds','CICSUST200.csv'))
+csv_text = File.read(Rails.root.join('lib','seeds','REL202.csv'))
 csv = CSV.parse(csv_text, headers: true)
 
 csv.each do |row|
