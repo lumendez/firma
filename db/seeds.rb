e31db9e6531d5dfc831cc5f28cd2b523d21fdef4
luis <luis@localhost.localdomain>
2021-05-22 21:25:16 -0500

    Carga 22 mayo 2021

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib','seeds','04mayo2021.csv'))
+csv_text = File.read(Rails.root.join('lib','seeds','22mayo21.csv'))
 csv = CSV.parse(csv_text, headers: true)
 
 csv.each do |row|
