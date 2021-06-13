fb08842365cfee50f2150997db236b87bf6aef9e
luis <luis@localhost.localdomain>
2021-06-12 19:15:15 -0500

    Correcion de ultima carga de datos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,16 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib','seeds','202ESIMES.csv'))
+csv_text = File.read(Rails.root.join('lib','seeds','12junio21.csv'))
 csv = CSV.parse(csv_text, headers: true)
 
 csv.each do |row|
-  t = ConstanciaDocumento.new
 
-  t.numero_registro = row['REGISTRO']
-  t.id = row['ID']
-  t.numero_relacion = row['RELACION']
-  t.numero_oficio = row['OFICIO']
-  t.codigo_prestatario = ""
-  t.clave_programa = ""
-  t.fecha = row['FECHA']
-  t.nombre = row['NOMBRE']
-  t.apellido_paterno = row['PATERNO']
-  t.apellido_materno = row['MATERNO']
-  t.boleta = row['BOLETA']
-  t.unidad_academica = row['UNIDAD']
-  t.programa_academico = row['CARRERA']
-  t.periodo = ""
-  t.prestatario = ""
-  t.constancia_emitida = row['EMITIDA']
-  t.user_id = row['USERID']
-  t.correo = row['CORREO']
+  id = row['ID']
+  oficio = row['OFICIO']
+  programa = row['CARRERA']
+  periodo = row['FECHA TEXTO']
+  prestatario = row['PRESTATARIO']
+  documento = ConstanciaDocumento.where(id: id)
+  documento.update(numero_oficio: oficio, programa_academico: programa, periodo: periodo, prestatario: prestatario)
 
-  t.save
 end
