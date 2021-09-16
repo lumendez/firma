461630d21fd93ae4bafdd720ed77e796c13446aa
luis <luis@localhost.localdomain>
2021-09-15 21:19:16 -0500

    Captura 13 de septiembre 2021 455 constancias

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib','seeds','27agosto21-2.csv'))
+csv_text = File.read(Rails.root.join('lib','seeds','13septiembre2021.csv'))
 csv = CSV.parse(csv_text, headers: true)
 
 csv.each do |row|
