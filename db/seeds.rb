1acf01dd549bc31ac3eb4829417b66cfbedc785c
luis <luis@localhost.localdomain>
2022-03-23 16:05:36 -0600

    Cambios en las condiciones para impresion de constancia por articulo 52 y carga de 57 constancias

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib','seeds','17marzo22.csv'))
+csv_text = File.read(Rails.root.join('lib','seeds','23marzo22.csv'))
 csv = CSV.parse(csv_text, headers: true)
 
 csv.each do |row|
