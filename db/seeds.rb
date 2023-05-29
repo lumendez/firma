f6c564008fda7c1a058f01a611b0b72e0b0e36dd
luis <luis@localhost.localdomain>
2023-05-29 09:49:40 -0600

    Se agregaron 421 constancias

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib','seeds','13mayo232.csv'))
+csv_text = File.read(Rails.root.join('lib','seeds','25mayo23.csv'))
 csv = CSV.parse(csv_text, headers: true)
 
 csv.each do |row|
