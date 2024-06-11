d6660c75fcff0157be199bc0464648918eaf9a45
luis <luis@localhost.localdomain>
2024-06-10 20:24:33 -0600

    Se agregaron 432 constancias de servicio social

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib','seeds','03junio24.csv'))
+csv_text = File.read(Rails.root.join('lib','seeds','10junio24.csv'))
 csv = CSV.parse(csv_text, headers: true)
 
 csv.each do |row|
