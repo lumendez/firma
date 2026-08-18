e3de9ae98601486b3bc4c3b092be8d46a00bba13
luis <luis@localhost.localdomain>
2026-08-18 10:19:05 -0600

    se cargaron 948 constancias

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib','seeds','22julio26.csv'))
+csv_text = File.read(Rails.root.join('lib','seeds','18agosto26.csv'))
 csv = CSV.parse(csv_text, headers: true)
 
 csv.each do |row|
